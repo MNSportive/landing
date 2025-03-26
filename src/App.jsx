c99398cad30ea2cf4566bc9df443a3093082388a
MNSportive <lilla.takacs.pte.ajk@gmail.com>
2025-03-26 16:01:16 -0700

    hide order components

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,20 @@ import { Products } from './components/Products'
 import { Info } from './components/Info'
 import { Order } from './components/Order'
 
+const showPrematureContent = false
+
 function App() {
   return (
     <div className="app-container flex column">
       <Header />
       <main className="flex column">
         <Info />
-        <Products />
-        <Order />
+        {showPrematureContent && (
+          <>
+            <Products />
+            <Order />
+          </>
+        )}
       </main>
       <Footer />
     </div>
