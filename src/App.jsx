56b50f458c707b61a84013d374d7cf269262b52c
MNSportive <lilla.takacs.pte.ajk@gmail.com>
2025-03-31 16:37:57 -0700

    wrap app in theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,32 @@ import { Products } from './components/Products'
 import { Info } from './components/Info'
 import { Order } from './components/Order'
 import { CartProvider } from './contexts/cartContext'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import theme from './styles/theme'
 
 const showPrematureContent = true
 
 function App() {
   return (
-    <CartProvider>
-      <div className="app-container flex column">
-        <Header />
-        <main className="flex column">
-          <Info />
-          {showPrematureContent && (
-            <>
-              <Products />
-              <Order />
-            </>
-          )}
-        </main>
-        <Footer />
-      </div>
-    </CartProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <CartProvider>
+        <div className="app-container flex column">
+          <Header />
+          <main className="flex column">
+            <Info />
+            {showPrematureContent && (
+              <>
+                <Products />
+                <Order />
+              </>
+            )}
+          </main>
+          <Footer />
+        </div>
+      </CartProvider>
+    </ThemeProvider>
   )
 }
 
