8807d2e12087b2c2c56852401e42e6f847a7be03
MNSportive <lilla.takacs.pte.ajk@gmail.com>
2025-03-31 16:38:26 -0700

    update header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,92 @@
+import React, { useState } from 'react'
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  Typography,
+  Button,
+  useTheme,
+  useMediaQuery,
+} from '@mui/material'
 import Logo from '../mns.svg'
-import { useState } from 'react'
 import { FadeInModal } from './Modal'
 
 export const Header = () => {
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+
   const [aboutOpen, setAboutOpen] = useState(false)
   const [FAQOpen, setFAQOpen] = useState(false)
+
   const handleAboutClose = () => setAboutOpen(false)
   const handleFAQClose = () => setFAQOpen(false)
+
   return (
-    <div className="header flex row">
-      <img
-        src={Logo}
-        className="header-logo"
-        alt="Mayotte Nutrition Sportive"
-      />
-      <nav className="flex">
-        <p onClick={setAboutOpen} style={{ textDecoration: 'underline' }}>
-          À propos
-        </p>
-        <p onClick={setFAQOpen} style={{ textDecoration: 'underline' }}>
-          FAQ
-        </p>
-      </nav>
-      <>
+    <AppBar
+      position="static"
+      color="transparent"
+      elevation={0}
+      sx={{
+        backgroundColor: theme.palette.background.default,
+        borderBottom: `1px solid ${theme.palette.secondary.main}`,
+      }}
+    >
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <Box
+          component="img"
+          src={Logo}
+          alt="Mayotte Nutrition Sportive"
+          sx={{
+            height: isMobile ? 40 : 60,
+            mr: 2,
+          }}
+        />
+
+        <Box display="flex" alignItems="center" gap={3}>
+          <Button
+            onClick={() => setAboutOpen(true)}
+            sx={{
+              color: theme.palette.primary.main,
+              fontWeight: 500,
+              textTransform: 'none',
+              fontSize: '1rem',
+              fontFamily: 'Roboto, sans-serif',
+              textDecoration: 'underline',
+              '&:hover': {
+                backgroundColor: 'transparent',
+                color: theme.palette.primary.dark,
+              },
+            }}
+          >
+            À propos
+          </Button>
+
+          <Button
+            onClick={() => setFAQOpen(true)}
+            sx={{
+              color: theme.palette.primary.main,
+              fontWeight: 500,
+              textTransform: 'none',
+              fontSize: '1rem',
+              fontFamily: 'Roboto, sans-serif',
+              textDecoration: 'underline',
+              '&:hover': {
+                backgroundColor: 'transparent',
+                color: theme.palette.primary.dark,
+              },
+            }}
+          >
+            FAQ
+          </Button>
+        </Box>
+
         <FadeInModal
           handleClose={handleAboutClose}
           open={aboutOpen}
           theme="about"
         />
         <FadeInModal handleClose={handleFAQClose} open={FAQOpen} theme="faq" />
-      </>
-    </div>
+      </Toolbar>
+    </AppBar>
   )
 }
